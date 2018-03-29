c3d0d689fad838bda517a9c0324761681b0cab33
Tri Retno Wulandari <37777244+triwulanr@users.noreply.github.com>
2018-03-29 08:33:16 +0700

    Add files via upload

---
+++ Rplmod8.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rplmod8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Rplmod8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Deret Fibonacci : ");
+        int n = show.nextInt();
+        long fib[] = new long[n];
+         
+        fib[0] = 0;
+        fib[1] = 1;
+         
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    }
+ 
+}
+    
+    
+
